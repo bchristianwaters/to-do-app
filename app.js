a6ec82d3a9065e1ad4cce3371f0e537e680a462c
Christian Waters <christianwaters1993@gmail.com>
2017-10-06 22:51:42 -0400

    Assignment 8 completed

--- app.js
+++ app.js
@@ -16,20 +16,36 @@ function onReady() {
     renderTheUI(toDos);
   }
 
+  function deleteToDo(newLi) {
+    toDos.splice(newLi.id, 1);
+    toDoList.removeChild(newLi);
+    renderTheUI(toDos);
+  }
+
   function renderTheUI(toDos) {
     const toDoList = document.getElementById('toDoList');
 
     toDoList.textContent = '';
+    let index = 0;
 
     toDos.forEach(function(toDo) {
       const newLi = document.createElement('li');
       const checkbox = document.createElement('input');
+      const deleteButton = document.createElement('button');
       checkbox.type = "checkbox";
+      deleteButton.type = "button";
+      deleteButton.textContent = "Delete";
 
       newLi.textContent = toDo.title;
+      newLi.id = index;
 
       toDoList.appendChild(newLi);
       newLi.appendChild(checkbox);
+      newLi.appendChild(deleteButton);
+      deleteButton.addEventListener('click', event => {
+        deleteToDo(newLi);
+      })
+      index ++;
     });
 
   }
